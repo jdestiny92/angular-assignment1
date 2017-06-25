00f3a4a2e0110c3c3494eff438648ba0e1a41614
Julian Biebelberg <jbiebelberg@gmail.com>
2017-06-25 12:37:29 -0400

    changed project to a filter project

--- app.js
+++ app.js
@@ -2,42 +2,16 @@
 
 'use strict';	
 
-angular.module('lunchChecker', [])
+angular.module('filterChecker', [])
 
-	.controller('checkLunch', function($scope){
-		$scope.data = '';
-		$scope.result = '';
-
-		$scope.convert = function(){
-			var data = $scope.data;
-
-			data = data.toString();
-
-			data = data.split(',');
-
-			for(var i=0; i<data.length; i++){
-				data[i] = data[i].trim();
-
-				if(data[i] == ''){
-					data.splice(i, 1);
-				};
-			}
-
-			if(data.length <= 3){
-				$scope.result = 'Enjoy!';
-			}
-
-			if(data.length > 3){
-				$scope.result = 'Too much!';
-			}
-			
-
-			if(data == ''){
-				$scope.result = 'Please enter data first.';
-			}
-
-			console.log(data);
-		}
+	.controller('checkFilter', function($scope){
+		
+		$scope.point = 0;
+		$scope.point2 = 100;
+		$scope.point3 = 100;
+		// set settings to their default / maximum values!
+		
+		
 	})
 
 
